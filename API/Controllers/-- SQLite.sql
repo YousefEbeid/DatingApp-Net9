1f339e8abb87f23ea86df50ac37702fbffd9c1bf
youse <youse@YOUSEF_EBEID>
2025-05-31 19:18:07 +0300

    Section 2

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,13 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,'Yousef Ebeid');
+
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,'Yousef Ragab');
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,'Mohamed');
+
+INSERT INTO Users (Id, UserName)
+VALUES (4,'Joo Ebeid');
